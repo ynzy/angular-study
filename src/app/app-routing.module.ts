430b5f34e78c446593ff4988cabe59b4b13f7792
zhangyong <zhangyong@163.com>
2019-06-27 16:23:53 +0800

    路由懒加载

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user',loadChildren:'./module/user/user.module#UserModule'
+  },{
+    path:'product',loadChildren:'./module/product/product.module#ProductModule'
+  },{
+    path:'article',loadChildren:'./module/article/article.module#ArticleModule'
+  },
+  {
+    path:'**',redirectTo:'user'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
