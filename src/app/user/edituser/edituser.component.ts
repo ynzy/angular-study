4296e8e58497f40fac7b3d248130dc23016c614a
zhangyong <zhangyong@163.com>
2019-06-28 14:16:20 +0800

    antdesign表单输入

--- src/app/user/edituser/edituser.component.ts
+++ src/app/user/edituser/edituser.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edituser.component.scss']
 })
 export class EdituserComponent implements OnInit {
+  //select框
+  options= [
+    { value: 'vip11', label: 'vip1' },
+    { value: 'vip22', label: 'vip2' }
+  
+  ] 
 
+  inputData:any={
+    inputValue:'',
+    age:''  ,
+    sex:'0',
+    checkOptions: [
+      { label: 'php', value: 'php', checked: true },
+      { label: 'java', value: 'java' },
+      { label: 'h5', value: 'h5'  }
+    ],
+    vipcheckedOption:this.options[0],
+    date:''
+  }
   constructor() { }
 
   ngOnInit() {
   }
-
+  onChange(e:Date) {
+    console.log(e)
+    var date = new Date(e)
+    console.log(date.getTime())
+    // this.inputData.date = date.getTime();
+  }
+  handleSubmit() {
+    console.log(this.inputData)
+  }
 }
