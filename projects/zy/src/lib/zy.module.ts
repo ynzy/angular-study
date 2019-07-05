ac959f29eb3f2fcff3793cb3d72062332b3dec02
zhangyong <zhangyong@163.com>
2019-07-05 10:33:17 +0800

    自定义类库组件

--- projects/zy/src/lib/zy.module.ts
+++ projects/zy/src/lib/zy.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { ZyComponent } from './zy.component';
 import { HeaderComponent } from './header/header.component';
 
+const USI = [
+  HeaderComponent,
+]
+
 @NgModule({
   declarations: [ZyComponent, HeaderComponent],
   imports: [
   ],
-  exports: [ZyComponent]
-})
+  exports: [ZyComponent, ...USI]
+}) 
 export class ZyModule { }
--- projects/zy/src/public-api.ts
+++ projects/zy/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/zy.service';
 export * from './lib/zy.component';
 export * from './lib/zy.module';
+//导出新建的组件
+export * from './lib/header/header.component';
